3241a1d3fa11ada41a060cce3d80c6d86f47035c
paul avram <avrampaulgabriel@gmail.com>
2026-05-27 14:27:11 +0300

    Updated hardcoded localhost URLs to Azure Cloud URLs

--- order-service/src/main/java/com/yachtsshop/orderservice/config/CorsConfig.java
+++ order-service/src/main/java/com/yachtsshop/orderservice/config/CorsConfig.java
@@ -19,7 +19,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:8081")
+                        .allowedOrigins(allowedOrigin)
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
